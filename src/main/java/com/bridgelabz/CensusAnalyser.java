6d433c3e464f5bbd7dc9c7e8e13adeadc50e781f
asimbinahammed <asim242370@gmail.com>
2021-10-05 09:54:57 +0530

    loading from csv file

--- src/main/java/com/bridgelabz/CensusAnalyser.java
+++ src/main/java/com/bridgelabz/CensusAnalyser.java
@@ -1,7 +1,30 @@
+/**
+ * @author-ASIM AHAMMED
+ */
+
 package com.bridgelabz;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CensusAnalyser {
-    public static void main(String[] args) {
-        System.out.println("Welcome to census Analyser");
+    public static void main(String[] args) throws IOException, StateSensorAnalyserException {
+        System.out.println("Welcome To The Program");
+        String filePathRead = ".src/main/resources/IndianStateCensusData.csv";
+        String fileName = "IndianStateCensusData";
+        String delimiter =",";
+        List<String> stringName = new ArrayList<>();
+        stringName.add("State");
+        stringName.add("Population");
+        stringName.add("AreaInSqKm");
+        stringName.add("DensityPerSqKm");
+
+        ReadOperations readObj = new ReadOperations();
+        int count = readObj.readDataCount(filePathRead, fileName);
+        System.out.println(count);
+
+        readObj.readDelimiter(filePathRead, delimiter);
+        readObj.readHeader(filePathRead, stringName);
     }
 }
